ebb359b5b2552c98cbdc07679ee8cf7913dfc5fc
tfevre <tfevre@metadev3.com>
2022-11-07 17:37:00 +0100

    error when trying to swap token2 to token1

--- README.md
+++ README.md
@@ -16,4 +16,6 @@ npx hardhat run scripts/deploy.js
 TO Do :
 - faire marcher la fonction create pair dans le contrat pool
 - faire les fees dans la fonction transfer de MyToken
+
+- Faire la fonction pour changer de token --> mais quels tokens exactement ???? exactment
 - Faire un front en localHost React pour juste changer de token et ajouter/retirer de la blacklist
--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -7,53 +7,44 @@ import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
 
 contract Pool is Ownable{
     MyToken public ERC20Token;
+    ERC20 public ERC20Token2;
     address public poolAddress;
   
-    address public token2Address = 0x094616F0BdFB0b526bD735Bf66Eca0Ad254ca81F;
-    address public constant ERC20TokenAddress = 0x9Edc5ec1Ec2ceF63df9696f9483a8f24Fa284035;
+    address public ERC20TokenAddress = 0xfB43c939B27D0Cb543Fe81B11F4a885a21b68215;
+    address public ERC20Token2Address = 0x281b37432e1Ce20AadEbD0fD786369D9F0Ce44D1;
     address public constant routerAddress = 0xD99D1c33F9fC3444f8101754aBC46c52416550D1;
     address public constant factoryAddress = 0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73;
     IUniswapV2Router02 private pancakeRouter = IUniswapV2Router02(routerAddress);
-    IUniswapV2Factory private factory = IUniswapV2Factory(factoryAddress);
-    address[] private blacklist;
+    IUniswapV2Factory private factory = IUniswapV2Factory(pancakeRouter.factory());
 
 
     constructor() {
         ERC20Token = MyToken(ERC20TokenAddress);
-        //ERC20Token.mint(address(this), 1000);
-        ERC20Token.approve(routerAddress, 1000);
+        ERC20Token2 = ERC20(ERC20Token2Address);
+        ERC20Token.approve(routerAddress, 10000);
+        ERC20Token2.approve(routerAddress, 10000);
     }
     
     function createPair() public onlyOwner {
-        poolAddress = factory.createPair(ERC20TokenAddress, token2Address);
+        if (factory.getPair(ERC20TokenAddress, ERC20Token2Address) == address(0)){
+            poolAddress = factory.createPair(ERC20TokenAddress, ERC20Token2Address);
+        }
     }
 
-    function changeToken2(address _address) public onlyOwner{
+    function changeTokenAddress(address _address) public onlyOwner{
         require(_address != address(0), "invalid address");
-        token2Address = _address;
-    }
-
-    function addBlacklist(address _ad) public onlyOwner {
-        blacklist.push(_ad);
-    }
-
-    function removeBlacklist(address _ad) public onlyOwner {
-        address[] memory tmpBlacklist = blacklist;
-        blacklist = new address[](0); // Reset the blacklist
-
-        for (uint i = 0; i < tmpBlacklist.length; i++){
-            if(tmpBlacklist[i] != _ad){
-                blacklist.push(tmpBlacklist[i]);
-            } 
+        ERC20TokenAddress = _address;
+        if (factory.getPair(ERC20TokenAddress, ERC20Token2Address) == address(0)){
+            poolAddress = factory.createPair(ERC20TokenAddress, ERC20Token2Address);
         }
     }
 
-    function isBlacklisted(address _ad) public view returns(bool){
-        for (uint i = 0; i < blacklist.length; i++){
-            if(blacklist[i] == _ad){
-                return true;
-            } 
+    function changeToken2Address(address _address) public onlyOwner{
+        require(_address != address(0), "invalid address");
+        ERC20Token2Address = _address;
+        if (factory.getPair(ERC20TokenAddress, ERC20Token2Address) == address(0)){
+            poolAddress = factory.createPair(ERC20TokenAddress, _address);
         }
-        return false;
     }
+
 }
