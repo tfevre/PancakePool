c0f12b73fe04afd97aeefe877b2657a6b18f469b
tfevre <tfevre@metadev3.com>
2022-11-15 14:14:03 +0100

    token2 ocntract update

--- contracts/MyToken2.sol
+++ contracts/MyToken2.sol
@@ -6,13 +6,9 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract MyToken2 is ERC20, Ownable {
-    address public teamAddress = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
-    address public liquidityPoolAddress = 0xdA020095671DA39d0c06c8f9d652C7deA3D77112;
 
-    constructor(address _teamAddress, address _liquidityPoolAddress) ERC20("MyToken2", "MTK2") {
+    constructor() ERC20("MyToken2", "MTK2") {
         mint(msg.sender, 10000);
-        teamAddress = _teamAddress;
-        liquidityPoolAddress = _liquidityPoolAddress;
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
