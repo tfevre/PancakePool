871be26cc9664eeffbc404d9a02d1a5d9a2ef722
tfevre <tfevre@metadev3.com>
2022-11-15 15:10:46 +0100

    deploy updated and contracts verified on bsc scan

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,7 @@ require('dotenv').config({ path: __dirname + '/.env' });
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.4",
-  defaultNetwork: "hardhat",
+  defaultNetwork: "bnb_testnet",
   networks: {
     hardhat: {
     },
@@ -14,5 +14,8 @@ module.exports = {
       gasPrice: 20000000000,
       accounts: [process.env.PRIVATE_KEY]
     }
-  }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_KEY,
+  },
 };
